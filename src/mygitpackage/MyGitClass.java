e60559d5abbc065f62a344ee245ba880f13847c2
Ahmet Demir <demir45@yahoo.com>
2021-04-18 17:09:42 +0300

    ilk commitim

---
+++ src/mygitpackage/MyGitClass.java
@@ -0,0 +1,13 @@
+package mygitpackage;
+
+public class MyGitClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("version 1");
+		
+		System.out.println("My second commit");
+
+	}
+
+}
